90be9563ceb42ab7a49f65fd980d010708561679
Mariano Madrid <marianomadrid.developer@gmail.com>
2024-08-04 11:29:19 +0200

    Modified the repeatString() function and removed .skip from the tests file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, numberOfLoops) {
+    let multipleWords = '';
+    for(let i = 0; i < numberOfLoops; i++){
+        multipleWords = multipleWords + word;
+    }
 
+    if(numberOfLoops < 0){
+        return "ERROR"
+    }
+    else{
+        return multipleWords;
+    }
 };
 
 // Do not edit below this line
