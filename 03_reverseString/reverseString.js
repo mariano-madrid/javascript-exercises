23e9ff6c5da27652663b5016f535d67a05dc0e75
Mariano Madrid <marianomadrid.developer@gmail.com>
2024-08-04 11:51:22 +0200

    Created reverseString() funciton and tested it

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(wordNotReversed) {
+    let arrayWordNotReversed = [];
+    let arrayWordReversed = [];
+    let wordReversed = '';
 
+    for(let i = 0; i < wordNotReversed.length; i++){
+        arrayWordNotReversed[i] = wordNotReversed.charAt(i);
+        arrayWordReversed[wordNotReversed.length - (i + 1)] = arrayWordNotReversed[i];
+    }
+
+    for(let i = 0; i < wordNotReversed.length; i++){
+        wordReversed += arrayWordReversed[i];
+    }
+
+    return wordReversed;
 };
 
 // Do not edit below this line
