a6c13b68efe5ac1ef8a9e126be7fd79f07eb6683
Mariano Madrid <marianomadrid.developer@gmail.com>
2024-08-04 14:12:40 +0200

    Created removeFromArray() function and tested it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    args.forEach((arg) =>{
+        let index = array.indexOf(arg)
+         
+        for(let i = 0; i < array.length; i++){
+            if(array[index] === array[index + 1]){
+                array.splice(index + 1, 1);
+            }
+        }
+        
+        if(!(index === -1 | String)){
+            array.splice(index, 1);
+        }
+    });
+
+    return array;
+
+
 };
 
 // Do not edit below this line
