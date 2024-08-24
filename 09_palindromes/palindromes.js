d009d33689fbe63d4fcaf85c8c0ebddf383d1e76
Mariano Madrid <marianomadrid.developer@gmail.com>
2024-08-24 19:37:44 +0200

    Exercise 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+string = string.toLowerCase();
+let normalArray = Array.from(string);
+let reversedArray = [];
+normalArray = normalArray.filter(spaces => spaces != ' ');
+normalArray = normalArray.filter(comas => comas != ',');
+normalArray = normalArray.filter(dots => dots != '.');
+normalArray = normalArray.filter(exclamation => exclamation != '!');
+
+
+for(let i = 0; i < normalArray.length; i++){
+    reversedArray[i] = normalArray[(normalArray.length - 1) - i];
+}
+
+if(normalArray.toString() === reversedArray.toString()){
+    return true;
+}
+else{
+    return false
+}
+
 
 };
 
