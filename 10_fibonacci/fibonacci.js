89d4ffc7e16e3224c1c5eb4ad13f38eb914eb00a
Mariano Madrid <marianomadrid.developer@gmail.com>
2024-08-25 17:59:22 +0200

    Exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+num = parseInt(num);
+let fiboNum = 0;
+let fibonacci = [];
+fibonacci[0] = 1;
+fibonacci[1] = 1;
+
+if(num === 0){
+    return 0;
+}
+else if (num < 0){
+    return 'OOPS'
+}
+else{
+    for(let i = 2; i < num; i++){
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+}
+
+fiboNum = parseInt(fibonacci[num - 1]);
+
+return fiboNum;
 
 };
 
